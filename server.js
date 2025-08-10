d1f1756dc63e195c782a5947c398360453d83247
Justin Remeny <1730reedvale@gmail.com>
2025-08-09 20:10:27 -0700

    Add /api/ping test route

--- server.js
+++ server.js
@@ -10,4 +10,5 @@ app.get('/', (req, res) => res.send('Backend is running!'));
 app.get('/health', (req, res) => res.status(200).send('OK'));
 
 const PORT = process.env.PORT || 3000;
+app.get('/api/ping', (req, res) => res.json({ pong: true }));
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
